ebb98ba57b796a2369ae5ff94f2524a3f61aedfc
Parimala-15 <133242784+Parimala-15@users.noreply.github.com>
2026-06-14 12:03:29 +0530

    Update channel_service.py

--- backend/app/services/channel_service.py
+++ backend/app/services/channel_service.py
@@ -4,7 +4,7 @@ import requests
 def send_to_channel_service():
 
     response = requests.post(
-        "http://127.0.0.1:8001/send"
-    )
+    "https://xeno-crm-channel.onrender.com/send"
+)
 
     return response.json()
